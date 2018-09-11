bc393db1544937b6e3b3b4c0e762367ddd30f17e
rayhanf <q8o1b@ugrad.cs.ubc.ca>
2018-09-11 11:06:49 -0700

    first task

---
+++ src/ui/Main.java
@@ -0,0 +1,29 @@
+package ui;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        numberofSets();
+        remainingRestTime();
+
+    }
+
+
+
+    public static void numberofSets() {
+        System.out.println("Good work! You did ... sets so far.");
+    }
+
+
+    public static void remainingRestTime() {
+        System.out.println("Rest! You have ... mins left.");
+    }
+
+}
+
+
+
+
+
